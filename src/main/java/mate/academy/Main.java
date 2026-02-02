308c6f38cd2024cb7c636fce8b7145ecf107d3e0
Andrii Lotoshynskyi <wetwettew@gmail.com>
2026-02-03 00:24:25 +0200

    Configured hibernate in hibernate.cfg.xml file; Added Movie model DAO and Service

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,18 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Title");
+        movie.setDescription("Description");
+        Movie savedMovie = movieService.add(movie);
+        Movie foundMovie = movieService.get(savedMovie.getId());
     }
 }
---
+++ src/main/java/mate/academy/service/MovieServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.service;
+
+import mate.academy.dao.MovieDao;
+import mate.academy.dao.MovieDaoImpl;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Movie;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    private MovieDao movieDao = new MovieDaoImpl();
+
+    @Inject
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public Movie get(Long id) {
+        return movieDao.get(id).orElseThrow();
+    }
+}
